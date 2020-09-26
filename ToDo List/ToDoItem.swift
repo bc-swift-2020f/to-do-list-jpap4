ffaf602a60698c058750459372d36a9665c5c49c
John Pappas <johncpappas4@gmail.com>
2020-09-26 19:23:42 -0400

    Completed Section 5.6

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by John Pappas on 9/26/20.
+//  Copyright © 2020 John Pappas. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
