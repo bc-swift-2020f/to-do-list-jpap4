e2a35faf8f8a6ad12914878b38ec0bd472f368a3
John Pappas <johncpappas4@gmail.com>
2020-09-26 13:00:41 -0400

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by John Pappas on 9/26/20.
+//  Copyright © 2020 John Pappas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
