465171d1ffc46c13442a810c31c91e20f72c48d3
John Pappas <johncpappas4@gmail.com>
2020-09-26 13:06:09 -0400

    Initial Commit

--- ToDo List/ToDoList.swift
+++ ToDo List/ToDoList.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ToDoList: UIViewController {
+@IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
